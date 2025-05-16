5f61f92222d0154051441c47f298680831817940
Saja386 <140643092+Saja386@users.noreply.github.com>
2025-05-17 00:01:57 +0200

    feat: Create Dash Animation

---
+++ Aura_UE_Version_5_3/Aura/Plugins/ThreepeatAnimTools/Intermediate/Build/Win64/UnrealEditor/Inc/ThreepeatAnimTools/UHT/CurveEditorShiftKeysFilter.generated.h
@@ -0,0 +1,54 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "CurveEditorShiftKeysFilter.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef THREEPEATANIMTOOLS_CurveEditorShiftKeysFilter_generated_h
+#error "CurveEditorShiftKeysFilter.generated.h already included, missing '#pragma once' in CurveEditorShiftKeysFilter.h"
+#endif
+#define THREEPEATANIMTOOLS_CurveEditorShiftKeysFilter_generated_h
+
+#define FID_Exports_PackagePlugins_tatools_2_0_1_UE54_ThreepeatAnimTools_HostProject_Plugins_ThreepeatAnimTools_Source_ThreepeatAnimTools_Public_CurveEditorShiftKeysFilter_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUCurveEditorShiftKeysFilter(); \
+	friend struct Z_Construct_UClass_UCurveEditorShiftKeysFilter_Statics; \
+public: \
+	DECLARE_CLASS(UCurveEditorShiftKeysFilter, UCurveEditorFilterBase, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/ThreepeatAnimTools"), NO_API) \
+	DECLARE_SERIALIZER(UCurveEditorShiftKeysFilter)
+
+
+#define FID_Exports_PackagePlugins_tatools_2_0_1_UE54_ThreepeatAnimTools_HostProject_Plugins_ThreepeatAnimTools_Source_ThreepeatAnimTools_Public_CurveEditorShiftKeysFilter_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	UCurveEditorShiftKeysFilter(UCurveEditorShiftKeysFilter&&); \
+	UCurveEditorShiftKeysFilter(const UCurveEditorShiftKeysFilter&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UCurveEditorShiftKeysFilter); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UCurveEditorShiftKeysFilter); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(UCurveEditorShiftKeysFilter) \
+	NO_API virtual ~UCurveEditorShiftKeysFilter();
+
+
+#define FID_Exports_PackagePlugins_tatools_2_0_1_UE54_ThreepeatAnimTools_HostProject_Plugins_ThreepeatAnimTools_Source_ThreepeatAnimTools_Public_CurveEditorShiftKeysFilter_h_12_PROLOG
+#define FID_Exports_PackagePlugins_tatools_2_0_1_UE54_ThreepeatAnimTools_HostProject_Plugins_ThreepeatAnimTools_Source_ThreepeatAnimTools_Public_CurveEditorShiftKeysFilter_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Exports_PackagePlugins_tatools_2_0_1_UE54_ThreepeatAnimTools_HostProject_Plugins_ThreepeatAnimTools_Source_ThreepeatAnimTools_Public_CurveEditorShiftKeysFilter_h_15_INCLASS_NO_PURE_DECLS \
+	FID_Exports_PackagePlugins_tatools_2_0_1_UE54_ThreepeatAnimTools_HostProject_Plugins_ThreepeatAnimTools_Source_ThreepeatAnimTools_Public_CurveEditorShiftKeysFilter_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> THREEPEATANIMTOOLS_API UClass* StaticClass<class UCurveEditorShiftKeysFilter>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Exports_PackagePlugins_tatools_2_0_1_UE54_ThreepeatAnimTools_HostProject_Plugins_ThreepeatAnimTools_Source_ThreepeatAnimTools_Public_CurveEditorShiftKeysFilter_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
